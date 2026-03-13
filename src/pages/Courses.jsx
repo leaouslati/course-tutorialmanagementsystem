fb1425cd68923ebee0efdb73a5ff513efedcc267
reemantar <reemantar301@gmail.com>
2026-03-14 01:18:32 +0200

    Changed some colors in coursecard and courses page,added the navbar in the courses page, added some images in the mockdata

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { courses } from "../data/mockdata.js";
+import { courses } from "../data/mockdata";
 import CourseCard from "../components/CourseCard";
+import Navbar from "../components/Navbar.jsx";
 
-function Courses() {
+function Courses({ isLoggedIn }) {
   return (
-    <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-      {courses.map((course) => (
-        <CourseCard key={course.id} course={course} />
-      ))}
+    <div className="min-h-screen bg-[#F4F8FD]">
+      {/* Navbar at the top */}
+      <Navbar isLoggedIn={isLoggedIn} light />
+
+      <div className="max-w-[1600px] mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {/* Page Title */}
+        <h1 className="text-4xl font-bold text-gray-900 mb-10 text-left">
+          Available Courses
+        </h1>
+
+        {/* Courses Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {courses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
