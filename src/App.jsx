bb3bcf102b04edc1658e53665e51f70dedff913e
reemnassif6 <rimfnassif@gmail.com>
2026-03-15 01:27:34 +0200

    managing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import {Routes, Route} from "react-router-dom";   
-//import CourseDetails from "./pages/CourseDetails.jsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Courses from "./pages/Courses.jsx";
+import ManageCourses from "./pages/ManageCourses.jsx";
+// Optional future pages
+// import CourseDetails from "./pages/CourseDetails.jsx";
 // import Enrollments from "./pages/Enrollments.jsx";
- import Home from "./pages/Home.jsx";
 // import Login from "./pages/Login.jsx";
 // import Register from "./pages/Register.jsx";
 // import Profile from "./pages/Profile.jsx";
-// import ManageCourses from "./pages/ManageCourses.jsx";
+
 import './index.css';
 
 function App() {
+  // Temporary admin check for testing
+  // Set isAdmin to true to test ManageCourses page
+  const isAdmin = true;
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} /> 
+      <Route path="/" element={<Home />} />
       <Route path="/courses" element={<Courses />} />
+
+      {/* ManageCourses route */}
+      <Route 
+        path="/manage-courses" 
+        element={
+          isAdmin ? <ManageCourses /> : <h2>Access Denied: Admins Only</h2>
+        } 
+      />
+
+      {/* Future routes */}
       {/* <Route path="/courses/:id" element={<CourseDetails />} />
       <Route path="/enrollments" element={<Enrollments />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/manage-courses" element={<ManageCourses />} /> */}
+      <Route path="/profile" element={<Profile />} /> */}
     </Routes>
   )
 }
 
-export default App
+export default App;
