080905641024a38b94379c175107335f1fb90747
reemnassif6 <rimfnassif@gmail.com>
2026-03-23 03:50:16 +0200

    Add Footer component with blue gradient, links, social icons and newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,28 @@ import Courses from "./pages/Courses.jsx";
 import Enrollments from "./pages/Enrollments.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
- import Profile from "./pages/Profile.jsx";
+import Profile from "./pages/Profile.jsx";
 import ManageCourses from "./pages/ManageCourses.jsx";
 import CourseDetails from "./pages/CourseDetails.jsx";
+import Footer from "./components/Footer.jsx";
 import './index.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/courses" element={<Courses />} />
-      <Route path="/enrollments" element={<Enrollments />} />
-      <Route path="/manage-courses" element={<ManageCourses />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/register" element={<Register />} />
-       { <Route path="/courses/:id" element={<CourseDetails />} /> }
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/enrollments" element={<Enrollments />} />
+        <Route path="/manage-courses" element={<ManageCourses />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/courses/:id" element={<CourseDetails />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
-    
-
 
 export default App;
