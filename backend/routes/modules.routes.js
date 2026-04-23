c04db6ad702f352d8b6cb092556cb7aa77758959
Intissar Soulaiman <intisarsulaimann@gmail.com>
2026-04-23 12:53:56 +0300

    Add delete module route

--- backend/routes/modules.routes.js
+++ backend/routes/modules.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { createModule } from '../controllers/modules.controller.js'
+import { createModule, deleteModule } from '../controllers/modules.controller.js'
 import { authMiddleware } from '../middleware/auth.middleware.js'
 import { instructorOnly } from '../middleware/instructorOnly.middleware.js'
 
 const router = Router()
 
 router.post('/', authMiddleware, instructorOnly, createModule)
+router.delete('/:id', authMiddleware, instructorOnly, deleteModule)
 
 export default router
